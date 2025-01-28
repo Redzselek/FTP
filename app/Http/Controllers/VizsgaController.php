3b72a0c3a53437351bc2849e087df2797815f749
Egyedi Róbert <egyedirobika@gmail.com>
2025-01-28 16:26:49 +0100

    Feltöltés és annak tesztelése

--- app/Http/Controllers/VizsgaController.php
+++ app/Http/Controllers/VizsgaController.php
@@ -16,7 +16,8 @@ use Exception;
 
 class VizsgaController extends Controller
 {
-    public function Regisztralas (Request $request) {
+    public function Regisztralas(Request $request)
+    {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
@@ -32,6 +33,7 @@ class VizsgaController extends Controller
         Auth::login($user);
 
         mail($user->email, $user->name . ' regisztráció sikeres volt', 'Sikeres regisztráció!"');
+
         return redirect('/vizsga/dashboard');
     }
 
@@ -46,13 +48,13 @@ class VizsgaController extends Controller
         return redirect('/vizsga/login');
     }
 
-    public function Login()
+    public function Bejelentkez()
     {
         return view('vizsga.login');
     }
 
     public function Bejelentkezes(Request $request)
-    { 
+    {
         $email = $request->input('email');
         $password = $request->input('password');
         if (Auth::attempt(['email' => $email, 'password' => $password])) {
@@ -70,8 +72,37 @@ class VizsgaController extends Controller
         if ($useradat) {
             $user = ['name' => $useradat->name, 'email' => $useradat->email];
         }
-        return view('vizsga.dashboard', [
-            'user' => $user
+        return view('vizsga.dashboard', ['user' => $user]);
+    }
+
+    public function Upload()
+    {
+        return view('vizsga.upload');
+    }
+
+    public function UploadManager(Request $request)
+    {
+        $data = $request->all();
+        $request->validate([
+            'file' => 'required|file|mimes:jpg,jpeg,png,webp|max:15360',
         ]);
+        if ($request->file()) {
+            $fileName = time() . '_' . $request->file->getClientOriginalName();
+            $destinationPath = public_path('uploads/vizsga');
+            $request->file('file')->move($destinationPath, $fileName);
+            
+            $useradat = Auth::user();
+            $upload = new VizsgaShows();
+            $upload->title = $data['title'];
+            $upload->description = $data['description'];
+            $upload->category = $data['category'];
+            $upload->type = $data['type'];
+            $upload->image_url = $fileName;
+            $upload->user_id = $useradat->id;
+            $upload->save();
+
+            return back()->with('success', 'Fájl sikeresen feltöltve.');
+        }
+        return back()->withErrors('A fájl feltöltése nem sikerült.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,44 +14,44 @@ Route::controller(BingoController::class)->group(function () {
     Route::get('/bingo/bingo','bingo')->name('bingo');
 });
 
-Route::controller(VizsgaremekController::class)->group(function () {
-    Route::get('/vizsgaremek/regisztracio', 'Regisztracio')->name('regisztracio');
-    Route::post('/vizsgaremek/regisztralas', 'Regisztralas')->name('regisztralas');
-    Route::get('/vizsgaremek/login', 'Login')->name('login');
-    Route::post('/vizsgaremek/bejelentkezes', 'Bejelentkezes')->name('bejelentkezes');
-    Route::get('/vizsgaremek/elfelejtett-jelszo', 'ElfelejtettJelszo')->name('password.request');
-    Route::post('/vizsgaremek/elfelejtett-jelszo', 'ElfelejtettJelszoKuldes')->name('password.email');
-    Route::get('/vizsgaremek/fooldal', 'Fooldal')->name('fooldal');
-});
+// Route::controller(VizsgaremekController::class)->group(function () {
+//     Route::get('/vizsgaremek/regisztracio', 'Regisztracio')->name('regisztracio');
+//     Route::post('/vizsgaremek/regisztralas', 'Regisztralas')->name('regisztralas');
+//     Route::get('/vizsgaremek/login', 'Login')->name('login');
+//     Route::post('/vizsgaremek/bejelentkezes', 'Bejelentkezes')->name('bejelentkezes');
+//     Route::get('/vizsgaremek/elfelejtett-jelszo', 'ElfelejtettJelszo')->name('password.request');
+//     Route::post('/vizsgaremek/elfelejtett-jelszo', 'ElfelejtettJelszoKuldes')->name('password.email');
+//     Route::get('/vizsgaremek/fooldal', 'Fooldal')->name('fooldal');
+// });
 
-Route::middleware(['auth'])->group(function () {
-    Route::controller(VizsgaremekController::class)->group(function () {
-        Route::get('/vizsgaremek/kijelentkezes', 'Kijelentkezes')->name('kijelentkezes');
-        Route::get('/vizsgaremek/jelszo-valtoztatas', 'JelszoValtoztatas')->name('valtoztatas');
-        Route::post('/vizsgaremek/jelszo-valtoztatas', 'JelszoValtoztatasMentes')->name('valtoztatas-mentes');
-        Route::get('/vizsgaremek/profil', 'Profil')->name('profil');
-        Route::post('/vizsgaremek/profil/nev-valtoztatas', 'NevValtoztatas')->name('nev.valtoztatas');
-        // CRUD műveletek
-        Route::get('/vizsgaremek/feltoltes', 'FeltoltesOldal')->name('feltoltesoldal');
-        Route::post('/vizsgaremek/feltoltes', 'FeltoltesKezelo')->name('feltoltes');
-        Route::get('/vizsgaremek/musorok', 'Musorok')->name('musorok');
-        Route::get('/vizsgaremek/musorok/tovabbinezet/{id}', 'MusorMegtekint')->name('musor.megtekint');
-        Route::post('/vizsgaremek/musor-szerkesztes', 'MusorSzerkesztes')->name('musor.szerkesztes');
-        Route::delete('/vizsgaremek/musor-torles', 'MusorTorles')->name('musor.torles');
-        Route::post('/vizsgaremek/musorok/ertekeles/{musor_id}', 'MusorErtekeles')->name('musor.ertekeles');
-        Route::post('/vizsgaremek/musorok/hozzaszolas/{musor_id}', 'Hozzaszolas')->name('hozzaszolas');
-        Route::post('/vizsgaremek/musorok/hozzaszolas/szerkesztes/{hozzaszolas_id}', 'KommentSzerkesztes')->name('hozzaszolas.szerkesztes');
-        Route::post('/vizsgaremek/musorok/hozzaszolas/torles/{hozzaszolas_id}', 'KommentTorles')->name('hozzaszolas.torles');
-        Route::post('/vizsgaremek/musor/{musor_id}/hozzaszolas', [VizsgaremekController::class, 'Hozzaszolas'])->name('hozzaszolas.store')->middleware('auth');        
-    });
-});
+// Route::middleware(['auth'])->group(function () {
+//     Route::controller(VizsgaremekController::class)->group(function () {
+//         Route::get('/vizsgaremek/kijelentkezes', 'Kijelentkezes')->name('kijelentkezes');
+//         Route::get('/vizsgaremek/jelszo-valtoztatas', 'JelszoValtoztatas')->name('valtoztatas');
+//         Route::post('/vizsgaremek/jelszo-valtoztatas', 'JelszoValtoztatasMentes')->name('valtoztatas-mentes');
+//         Route::get('/vizsgaremek/profil', 'Profil')->name('profil');
+//         Route::post('/vizsgaremek/profil/nev-valtoztatas', 'NevValtoztatas')->name('nev.valtoztatas');
+//         // CRUD műveletek
+//         Route::get('/vizsgaremek/feltoltes', 'FeltoltesOldal')->name('feltoltesoldal');
+//         Route::post('/vizsgaremek/feltoltes', 'FeltoltesKezelo')->name('feltoltes');
+//         Route::get('/vizsgaremek/musorok', 'Musorok')->name('musorok');
+//         Route::get('/vizsgaremek/musorok/tovabbinezet/{id}', 'MusorMegtekint')->name('musor.megtekint');
+//         Route::post('/vizsgaremek/musor-szerkesztes', 'MusorSzerkesztes')->name('musor.szerkesztes');
+//         Route::delete('/vizsgaremek/musor-torles', 'MusorTorles')->name('musor.torles');
+//         Route::post('/vizsgaremek/musorok/ertekeles/{musor_id}', 'MusorErtekeles')->name('musor.ertekeles');
+//         Route::post('/vizsgaremek/musorok/hozzaszolas/{musor_id}', 'Hozzaszolas')->name('hozzaszolas');
+//         Route::post('/vizsgaremek/musorok/hozzaszolas/szerkesztes/{hozzaszolas_id}', 'KommentSzerkesztes')->name('hozzaszolas.szerkesztes');
+//         Route::post('/vizsgaremek/musorok/hozzaszolas/torles/{hozzaszolas_id}', 'KommentTorles')->name('hozzaszolas.torles');
+//         Route::post('/vizsgaremek/musor/{musor_id}/hozzaszolas', [VizsgaremekController::class, 'Hozzaszolas'])->name('hozzaszolas.store')->middleware('auth');        
+//     });
+// });
 
 
 
 Route::controller(VizsgaController::class)->group(function () {
-    Route::get('/vizsga/regisztracio', 'Regisztracio')->name('vizsga.regisztracio');
+    Route::get('/vizsga/register', 'Regisztracio')->name('vizsga.register');
     Route::post('/vizsga/regisztralas', 'Regisztralas')->name('vizsga.regisztralas');
-    Route::get('/vizsga/login', 'Login')->name('vizsga.login');
+    Route::get('/vizsga/login', 'Bejelentkez')->name('vizsga.login');
     Route::post('/vizsga/bejelentkezes', 'Bejelentkezes')->name('vizsga.bejelentkezes');
     Route::get('/vizsga/kijelentkezes', 'Kijelentkezes')->name('vizsga.kijelentkezes');
 
@@ -60,5 +60,7 @@ Route::controller(VizsgaController::class)->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::controller(VizsgaController::class)->group(function () {
         Route::get('/vizsga/dashboard', 'Dashboard')->name('vizsga.dashboard');
+        Route::get('/vizsga/upload', 'Upload')->name('vizsga.upload.form');
+        Route::post('/vizsga/uploadmanager', 'UploadManager')->name('vizsga.uploadManager');
     });
 });
